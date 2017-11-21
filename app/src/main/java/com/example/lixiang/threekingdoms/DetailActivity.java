85be697e47f96f2a3f79eb815bf84cbefe5f3002
KinomotoSakura <1796068989@qq.com>
2017-11-22 02:42:32 +0800

    origin_v7

--- app/src/main/java/com/example/lixiang/threekingdoms/DetailActivity.java
+++ app/src/main/java/com/example/lixiang/threekingdoms/DetailActivity.java
@@ -4,6 +4,10 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.animation.Animator;
+import android.animation.AnimatorInflater;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.AnimatorSet;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.ImageView;
@@ -11,6 +15,7 @@ import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.widget.FrameLayout;
 
 import org.greenrobot.eventbus.EventBus;
 
@@ -23,8 +28,16 @@ public class DetailActivity extends AppCompatActivity {
     private TextView character_origin;
     private TextView character_force;
     private ImageView favorite;
-    private ImageView back_to_card;
     private ImageView back_to_main;
+
+    private FrameLayout mFlContainer;
+    private FrameLayout mFlCardBack;
+    private FrameLayout mFlCardFront;
+
+    private AnimatorSet mRightOutSet; // 右出动画
+    private AnimatorSet mLeftInSet; // 左入动画
+
+    private boolean mIsShowBack;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -32,6 +45,10 @@ public class DetailActivity extends AppCompatActivity {
         getSupportActionBar().hide();
         getWindow().setFlags(WindowManager.LayoutParams. FLAG_FULLSCREEN , WindowManager.LayoutParams. FLAG_FULLSCREEN);
         setContentView(R.layout.activity_detail);
+
+        mFlContainer = (FrameLayout) findViewById(R.id.character_details);
+        mFlCardBack = (FrameLayout) findViewById(R.id.main_fl_card_back);
+        mFlCardFront = (FrameLayout) findViewById(R.id.main_fl_card_front);
 
         character_img = (ImageView) findViewById(R.id.character_img);
         character_name = (TextView) findViewById(R.id.character_name);
@@ -79,28 +96,6 @@ public class DetailActivity extends AppCompatActivity {
             }
         });
 
-        final RelativeLayout card = (RelativeLayout) findViewById(R.id.card);
-        final LinearLayout introduction = (LinearLayout) findViewById(R.id.introduction);
-        //设置初始状态
-        card.setVisibility(View.VISIBLE);
-        introduction.setVisibility(View.INVISIBLE);
-        character_img.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                card.setVisibility(View.INVISIBLE);
-                introduction.setVisibility(View.VISIBLE);
-            }
-        });
-
-        back_to_card = (ImageView) findViewById(R.id.back_to_card);
-        back_to_card.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                card.setVisibility(View.VISIBLE);
-                introduction.setVisibility(View.INVISIBLE);
-            }
-        });
-
         back_to_main = (ImageView) findViewById(R.id.back_to_main);
         back_to_main.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -108,5 +103,55 @@ public class DetailActivity extends AppCompatActivity {
                 finish();
             }
         });
+
+        setAnimators(); // 设置动画
+        setCameraDistance(); // 设置镜头距离
     }
+
+    // 设置动画
+    private void setAnimators() {
+        mRightOutSet = (AnimatorSet) AnimatorInflater.loadAnimator(this, R.animator.anim_out);
+        mLeftInSet = (AnimatorSet) AnimatorInflater.loadAnimator(this, R.animator.anim_in);
+
+        // 设置点击事件
+        mRightOutSet.addListener(new AnimatorListenerAdapter() {
+            @Override public void onAnimationStart(Animator animation) {
+                super.onAnimationStart(animation);
+                mFlContainer.setClickable(false);
+            }
+        });
+        mLeftInSet.addListener(new AnimatorListenerAdapter() {
+            @Override public void onAnimationEnd(Animator animation) {
+                super.onAnimationEnd(animation);
+                mFlContainer.setClickable(true);
+            }
+        });
+    }
+
+    // 改变视角距离, 贴近屏幕
+    private void setCameraDistance() {
+        int distance = 16000;
+        float scale = getResources().getDisplayMetrics().density * distance;
+        mFlCardFront.setCameraDistance(scale);
+        mFlCardBack.setCameraDistance(scale);
+    }
+
+    // 翻转卡片
+    public void flipCard(View view) {
+        // 正面朝上
+        if (!mIsShowBack) {
+            mRightOutSet.setTarget(mFlCardFront);
+            mLeftInSet.setTarget(mFlCardBack);
+            mRightOutSet.start();
+            mLeftInSet.start();
+            mIsShowBack = true;
+        } else { // 背面朝上
+            mRightOutSet.setTarget(mFlCardBack);
+            mLeftInSet.setTarget(mFlCardFront);
+            mRightOutSet.start();
+            mLeftInSet.start();
+            mIsShowBack = false;
+        }
+    }
+
 }
